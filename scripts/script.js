fe80c88a109d98cd65f0e9d3ec4039ac5e6863d8
Isaac Willian <isaaacwillian@gmail.com>
2022-01-13 18:25:33 -0300

    Adicionado a lógica de clicar dois p virar e dar certo e tals

--- scripts/script.js
+++ scripts/script.js
@@ -2,15 +2,12 @@ const front = "card_front";
 const back = "card_back";
 const icon = "icon";
 
-let techs = ['bootstrap', 'css', 'electron', 'firebase', 'html', 'javascript', 'jquery', 'mongo', 'node', 'react'];
 
-let cards = null;
 
 startGame();
 
 function startGame() {
-    cards = createCardsFromTechs(techs);
-    cards.sort(() => 0.5 - Math.random())
+    cards = game.createCardsFromTechs();
 
     initializeCards(cards);
 }
@@ -53,28 +50,27 @@ function createCardFace(face, card, element) {
     }
     element.appendChild(cardElementFace);
 }
-function createCardsFromTechs(techs) {
-    let cards = [];
 
-    for (let tech of techs) {
-        cards.push(createPairFromTech(tech));
-    }
-    return cards.flat();
-}
-function createPairFromTech(tech) {
-    return [{
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false,
-    }, {
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false,
-    }]
-}
-function createIdWithTech(tech) {
-    return tech + parseInt(Math.random() * 100);
-}
 function flipCard() {
-    this.classList.add("flip");
+
+    if (game.setCard(this.id)) {
+
+        this.classList.add("flip");
+        if (game.secondCard) {
+            if (game.checkMatch()) {
+                game.clearCards();
+            } else {
+                setTimeout(() => {
+                    let firstCardView = document.getElementById(game.firstCard.id);
+                    let secondCardView = document.getElementById(game.secondCard.id);
+
+                    firstCardView.classList.remove('flip');
+                    secondCardView.classList.remove('flip');
+                    game.unflipCards();
+                }, 1000);
+            }
+        }
+
+    }
+
 }
